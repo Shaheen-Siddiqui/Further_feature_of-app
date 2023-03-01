3f8c1d849612888fab362c9901eaf607b7ccc9d5
Shaheen-Siddiqui <shanvisiddya@gmail.com>
2023-03-01 19:37:58 +0530

    added category. component file

---
+++ src/router/category/category.component.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from "../../components/product-card/product-card.component";
+import { useParams } from 'react-router-dom';
+import "./category.style.scss"
+import { categoriesMapSelector } from "../../store/category/category.selector";
+import { useSelector } from 'react-redux';
+
+const Category = () => {
+    const categoriesMap =  useSelector(categoriesMapSelector)
+    const { category } = useParams()
+    const [products, setProduct] = useState(categoriesMap[category]);
+
+
+    useEffect(() => {
+        setProduct(categoriesMap[category]);
+    }, [categoriesMap, category]);
+
+
+    return (
+        <>
+            <h3 className='titles'>{category.toLocaleUpperCase()}</h3>
+            <div className='category-container'>
+
+                {products &&
+                    products.map((products) => {
+                        return <ProductCard key={products.id} products={products} />
+                    })
+                }
+            </div>
+        </>
+    )
+};
+export default Category
